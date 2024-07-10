5db2007fb8f526e10630069bb5f5143ead7a4b71
Matthew A Rosales <matthewrosales@usf.edu>
2024-07-09 20:48:08 -0400

    profile.js
    not sure if it needed to be each person or a spotlight

---
+++ profile.js
@@ -0,0 +1,14 @@
+//U22978120
+
+import React from 'react';
+
+function Profile({ name, role, }) { 
+  return (
+    <div className="profile">
+      <h2>{Kendrick Lamar}</h2>
+      <p>{Security guard}</p>
+    </div>
+  );
+}
+
+export default Profile;
